88f6dd54633b7996c07bb9831353651001bc2c74
Zach Baker <zach.r.baker1@gmail.com>
2017-01-11 18:41:35 -0700

    🐍

--- main.py
+++ main.py
@@ -7,3 +7,44 @@
 # zach = 'me'
 # print(zach);
 
+# myFloat = 7.0
+# print(myFloat)
+# anothaFloat = float(9)
+# print(anothaFloat)
+
+# one = 1
+# two = 2
+# three = one + two
+# print(three)
+
+# hello = "hello"
+# world = "world"
+# helloworld = hello + " " + world
+# print(helloworld)
+
+# a, b, c = 1, 2, 3;
+# print(a,b,c);
+
+# # change this code
+# mystring = 'hello'
+# myfloat = 10.0
+# myint = 20
+
+# # testing code
+# if mystring == "hello":
+#     print("String: %s" % mystring)
+# if isinstance(myfloat, float) and myfloat == 10.0:
+#     print("Float: %f" % myfloat)
+# if isinstance(myint, int) and myint == 20:
+#     print("Integer: %d" % myint)
+
+
+# def ShortenName(name):
+#     guy = name.split(', ')[0]
+#     dude = name.split(', ')[1]
+#     dude = dude.split(' ')
+#     if len(dude) == 1:
+#         return name
+#     dude[len(dude)-1] = dude[len(dude)-1][0] + '.'
+#     dude = ' '.join(dude)
+#     return guy + ', ' + dude
